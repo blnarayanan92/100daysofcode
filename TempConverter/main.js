6e09084feb27cf2611afc7c388175857fff401c4
Lakshminarayanan B <blnarayanan92@gmail.com>
2018-01-13 22:20:34 +0530

    adding temp converter

---
+++ TempConverter/main.js
@@ -0,0 +1,13 @@
+var one = document.querySelector('.field');
+var button = document.querySelector('.getButton');
+
+button.addEventListener('click', function(){
+    var first = parseInt(one.value);
+    var result = (first - 32) / 1.8;
+    if (first !== NaN){
+        alert(result);
+    }
+    else{
+        alert("You need to enter a value");
+    }
+});
