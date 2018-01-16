fd0826d2bebbec374fde614ca7a6fcc28038af50
Lakshminarayanan B <blnarayanan92@gmail.com>
2018-01-16 23:22:10 +0530

    keeping up with the streak- built nothing

--- CharacterCount/main.js
+++ CharacterCount/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-   var text_max = 199;
+$(document).ready(function () {
+    var text_max = 199;
     $('#feedback').html(text_max + 'Characters remaining');
-    
-    $('#textarea').keyup(function(){
+
+    $('#textarea').keyup(function () {
         var text_length = $('#textarea').val().length;
         var text_remaining = text_max - text_length;
-        
+
         $('#feedback').html(text_remaining + 'Characters remaining');
     });
 });
